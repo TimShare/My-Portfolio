610c3f82cbd65fdcb316d39731942ad2c6e71e85
TimShare <nbvtim43@gmail.com>
2024-09-20 17:45:14 +0300

    someone update

--- src/Helpers/ProjectList.js
+++ src/Helpers/ProjectList.js
@@ -1,11 +1,17 @@
-import project01 from "./../img/projects/01.jpg";
+import project01 from "./../img/projects/01-big.jpg";
 import project01Big from "./../img/projects/01-big.jpg";
 
-import project02 from "./../img/projects/02.jpg";
+import project02 from "./../img/projects/02-big.jpg";
 import project02Big from "./../img/projects/02-big.jpg";
 
-import project03 from "./../img/projects/03.jpg";
+import project03 from "./../img/projects/03-big.jpg";
 import project03Big from "./../img/projects/03-big.jpg";
+
+import project04 from "./../img/projects/04-big.png";
+import project04Big from "./../img/projects/04-big.png";
+
+import project05 from "./../img/projects/05-big.png";
+import project05Big from "./../img/projects/05-big.png";
 
 const projects = [
   {
@@ -32,15 +38,15 @@ const projects = [
   {
     title: "My-KAI",
     skills: "Python, Telebot, Sqlite3, Pillow",
-    img: project03,
-    imgBig: project03Big,
+    img: project04,
+    imgBig: project04Big,
     gitHubLink: "https://github.com/TimShare/My-KAI",
   },
   {
     title: "My-Parsers",
     skills: "Python, Openpyxl, Selenium",
-    img: project03,
-    imgBig: project03Big,
+    img: project05,
+    imgBig: project05Big,
     gitHubLink: "https://github.com/TimShare/My-Parsers",
   },
 ];
